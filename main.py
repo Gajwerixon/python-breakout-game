b20da1b3e776aec768d5ca33b21fbf7ecaa6dc53
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-04-28 21:17:40 +0200

    feat: init game class

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 import pygame
+from settings import *
+from sys import exit
+
+# pygame setup
+class Game:
+    """Main game class"""
+    def __init__(self):
+        # --- Basic setup ---   
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Breakout')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.game_active = False
+
+    def run_game(self):
+        """Main game loop"""
+        while self.running:
+            dt = self.clock.tick(60) / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # --- Draw and Refresh screen ---
+            self.screen.fill('black')
+            pygame.display.flip()
+        
+        # --- Quit game ---
+        pygame.quit()
+        exit()
+    
+game = Game()
+game.run_game()
